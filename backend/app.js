c77e02f68353fbc58d613a5ed15eeb8f9b65f6f4
GentileLuca1 <148053779+GentileLuca1@users.noreply.github.com>
2024-10-25 12:12:24 +0000

    esercizio quasi terminato

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name; // Legge il cookie "name"
+    if (name) {
+        // Se il cookie esiste, mostra la pagina di saluto
+        res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 }); 
+        res.render('greet', { message:'Benvenuto', name: name });
+    } else {
+        // Se non esiste, mostra il form
+        res.render('form');
+    }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
+    res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 }); 
     res.render('greet', { name: name });
 });
 
